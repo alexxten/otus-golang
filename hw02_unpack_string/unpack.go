97b342631fce42c04ba3750fe93c92c32da49f9c
alexxten <alexxten13@gmail.com>
2022-03-21 22:48:03 +0300

    hw2 - add success strings processing

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -1,12 +1,53 @@
 package hw02unpackstring
 
 import (
-	"errors"
+    "errors"
+    "fmt"
+	"strings"
+	"unicode"
+	"strconv"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(input_string string) (string, error) {
+    var initial_str = input_string
+    var formatted_str strings.Builder
+
+    var arr = make([]string, 0)
+    for i, v := range initial_str {
+        switch {
+        case i == len(initial_str)-1:
+            if !unicode.IsDigit(v) {
+                arr = append(arr, string(v))
+            }
+        case unicode.IsDigit(v):
+            continue
+        case unicode.IsDigit(rune(initial_str[i+1])):
+            arr = append(arr, initial_str[i:i+2])
+
+        default:
+            arr = append(arr, string(v))
+        }
+
+    }
+    fmt.Println(arr)
+    //switch {
+    //case unicode.IsDigit(rune(initial_str[0])):
+      //  fmt.Println("некорректно")
+    //}
+    //case unicode.IsDigit(v) && unicode.IsDigit(rune(initial_str[i+1])):
+    //		fmt.Println("некорректно")
+
+    for _, v := range arr {
+        switch {
+        case len(v) == 2:
+            repeat, _ := strconv.Atoi(string(v[1]))
+            formatted_str.WriteString(strings.Repeat(string(v[0]), repeat))
+        default:
+            formatted_str.WriteString(string(v))
+        }
+    }
+
+	return formatted_str.String(), nil
 }
