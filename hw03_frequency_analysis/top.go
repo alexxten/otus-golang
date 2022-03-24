a0f46774348c7176113c2a319c34b288a9e9e460
alexxten <alexxten13@gmail.com>
2022-03-24 20:13:04 +0300

    hw3 - add Top10 func

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,43 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+type wordsCount struct {
+	Key   string
+	Value int
+}
+
+var regularExpression = regexp.MustCompile(`[\p{L}\d]*[-]*[$\p{L}\d]+`)
+
+func Top10(inputString string) []string {
+	input := strings.ToLower(inputString)
+	if len(input) == 0 {
+		return make([]string, 0)
+	}
+	matches := regularExpression.FindAllString(input, -1)
+
+	counts := make(map[string]int)
+	for _, v := range matches {
+		counts[v]++
+	}
+
+	countsStruct := make([]wordsCount, 0)
+	for i, v := range counts {
+		countsStruct = append(countsStruct, wordsCount{i, v})
+	}
+	sort.SliceStable(countsStruct, func(i, j int) bool {
+		if countsStruct[i].Value == countsStruct[j].Value {
+			return countsStruct[i].Key < countsStruct[j].Key
+		}
+		return countsStruct[i].Value > countsStruct[j].Value
+	})
+	words := make([]string, 0)
+	for _, v := range countsStruct[:10] {
+		words = append(words, v.Key)
+	}
+	return words
 }
