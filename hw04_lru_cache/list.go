68e54fe625e5970c66cd97293c598a9745a95af5
alexxten <alexxten13@gmail.com>
2022-10-03 23:20:48 +0300

    add list methods

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,10 +17,87 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	head   *ListItem
+	tail   *ListItem
+	length int
 }
 
 func NewList() List {
 	return new(list)
+}
+
+func (l *list) Len() int {
+	return l.length
+}
+
+func (l *list) Front() *ListItem {
+	return l.head
+}
+
+func (l *list) Back() *ListItem {
+	return l.tail
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	newItem := &ListItem{Value: v}
+	switch {
+	case l.head == nil:
+		l.head = newItem
+		l.tail = newItem
+	case l.head != nil:
+		head := l.head
+		head.Prev = newItem
+		newItem.Next = head
+		l.head = newItem
+	}
+	l.length += 1
+	return l.head
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	newItem := &ListItem{Value: v}
+	switch {
+	case l.tail == nil:
+		l.head = newItem
+		l.tail = newItem
+	case l.tail != nil:
+		tail := l.tail
+		tail.Next = newItem
+		newItem.Prev = tail
+		l.tail = newItem
+	}
+	l.length += 1
+	return l.tail
+}
+
+func (l *list) Remove(i *ListItem) {
+	switch {
+	case l.Front() == i:
+		l.head = i.Next
+		l.head.Prev = nil
+	case l.Back() == i:
+		l.tail = i.Prev
+		l.tail.Next = nil
+	default:
+		prev := i.Prev
+		next := i.Next
+		prev.Next = next
+		next.Prev = prev
+	}
+	l.length -= 1
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	switch {
+	case l.head == i:
+	default:
+		// delete item from list
+		l.Remove(i)
+		// change pointers
+		i.Next = l.head
+		i.Prev = nil
+		l.head = i
+		// add +1 to list as we just moved it
+		l.length += 1
+	}
 }
