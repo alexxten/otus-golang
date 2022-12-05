e9be0aba4829a58d32527127244de7c2f267ca63
alexxten <alexxten13@gmail.com>
2022-12-05 22:41:54 +0300

    get file size

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,45 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrEmptyFile             = errors.New("zero file size")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
-	return nil
+	// get source file size
+	fileInfo, err := os.Stat(fromPath)
+	if err != nil {
+		return err
+	}
+	fileSize := fileInfo.Size()
+	// check file size
+	if fileSize == 0 {
+		return ErrEmptyFile
+	}
+	// check offset val
+	if offset > fileSize {
+		return ErrOffsetExceedsFileSize
+	}
+
+	src, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	dst, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	nBytes, err := io.Copy(dst, src)
+	print(nBytes)
+	return err
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -3,5 +3,23 @@ package main
 import "testing"
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("positive case", func(t *testing.T) {
+		err := Copy(
+			"testdata/input.txt",
+			"testdata/input2.txt",
+			0,
+			100,
+		)
+		print(err)
+	})
+
+	t.Run("zero file size", func(t *testing.T) {
+		err := Copy(
+			"/dev/urandom",
+			"testdata/input2.txt",
+			0,
+			100,
+		)
+		print(err)
+	})
 }
