2c0ea3798c5c6b5ab574c36dd1172b10a5224a17
alexxten <alexxten13@gmail.com>
2023-05-14 20:31:37 +0300

    hw08 - replace success test

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"io/ioutil"
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -9,18 +11,28 @@ import (
 )
 
 func TestRunCmd(t *testing.T) {
-	t.Run("testdata, success", func(t *testing.T) {
-		env, err := ReadDir("testdata/env")
+	t.Run("success", func(t *testing.T) {
+		dir, err := os.MkdirTemp("testdata", "tmp")
 		require.NoError(t, err)
-		err = os.Chmod("test.sh", 0o777)
+		defer os.RemoveAll(dir)
+		err = os.Mkdir(filepath.Join(dir, "vars"), 0o777)
+		require.NoError(t, err)
+		err = ioutil.WriteFile(filepath.Join(dir, "vars", "FOO"), []byte("value from file"), 0o666)
+		require.NoError(t, err)
+		err = ioutil.WriteFile(filepath.Join(dir, "t.sh"), []byte("#!/usr/bin/env bash\necho $1\necho $FOO\n"), 0o666)
+		require.NoError(t, err)
+		err = os.Chmod(filepath.Join(dir, "t.sh"), 0o777)
+		require.NoError(t, err)
+
+		env, err := ReadDir(filepath.Join(dir, "vars"))
 		require.NoError(t, err)
 
 		var returnCode int
 		result := capturer.CaptureStdout(func() {
-			returnCode = RunCmd([]string{"sh", "test.sh"}, env)
+			returnCode = RunCmd([]string{filepath.Join(dir, "t.sh"), "value from cmd"}, env)
 		})
 		require.Equal(t, 0, returnCode)
-		require.Equal(t, "PASS\n", result)
+		require.Equal(t, "value from cmd\nvalue from file\n", result)
 	})
 
 	t.Run("error executing command", func(t *testing.T) {
